74375be7fd6b61d80dde02b52459f6d8bc7b85ce
Ramkezh Premarajan <ramkezhprem7@gmail.com>
2022-07-21 02:33:10 +0530

    Add files via upload

---
+++ components/one.js
@@ -0,0 +1,12 @@
+function B1(){
+
+    return`
+    <span id="sp1">Browse job by Companies</span> <div id="searchbar"><form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+        <input type="text" placeholder="Search.." name="search2">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+      </div>`
+}
+
+export default B1;
+
---
+++ components/two.js
@@ -0,0 +1,34 @@
+function tab(){
+
+    return`
+    <a class="tabs"href="./main.html" target="_blank">Top 1000</a>
+    <a class="tabs"href="http://" target="_blank">A</a>
+    <a class="tabs"href="http://" target="_blank">B</a>
+    <a class="tabs"href="http://" target="_blank">C</a>
+    <a class="tabs"href="http://" target="_blank">D</a>
+    <a class="tabs"href="http://" target="_blank">E</a>
+    <a class="tabs"href="http://" target="_blank">F</a>
+    <a class="tabs"href="http://" target="_blank">G</a>
+    <a class="tabs"href="http://" target="_blank">H</a>
+    <a class="tabs"href="http://" target="_blank">I</a>
+    <a class="tabs"href="http://" target="_blank">J</a>
+    <a class="tabs"href="http://" target="_blank">K</a>
+    <a class="tabs"href="http://" target="_blank">L</a>
+    <a class="tabs"href="http://" target="_blank">M</a>
+    <a class="tabs"href="http://" target="_blank">N</a>
+    <a class="tabs"href="http://" target="_blank">O</a>
+    <a class="tabs"href="http://" target="_blank">P</a>
+    <a class="tabs"href="http://" target="_blank">Q</a>
+    <a class="tabs"href="http://" target="_blank">R</a>
+    <a class="tabs"href="http://" target="_blank">S</a>
+    <a class="tabs"href="http://" target="_blank">T</a>
+    <a class="tabs"href="http://" target="_blank">U</a>
+    <a class="tabs"href="http://" target="_blank">V</a>
+    <a class="tabs"href="http://" target="_blank">W</a>
+    <a class="tabs"href="http://" target="_blank">X</a>
+    <a class="tabs"href="http://" target="_blank">Y</a>
+    <a class="tabs"href="http://" target="_blank">Z</a>
+    <a class="tabs"href="http://" target="_blank">0-9</a>`
+  }
+  
+  export default tab;
