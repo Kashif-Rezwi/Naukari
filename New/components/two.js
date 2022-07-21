a3c428ca33f890f825cd933dc36518cdf5f30915
Ramkezh Premarajan <ramkezhprem7@gmail.com>
2022-07-21 22:23:10 +0530

    Add files via upload

---
+++ New/components/two.js
@@ -0,0 +1,34 @@
+function tab(){
+
+    return`
+    <a class="tabs"href="./main.html" target="_self">Top 1000</a>
+    <a class="tabs"href="./extra/A.html" target="_self">A</a>
+    <a class="tabs"href="./extra/B.html" target="_self">B</a>
+    <a class="tabs"href="./extra/C.html" target="_self">C</a>
+    <a class="tabs"href="./extra/D.html" target="_self">D</a>
+    <a class="tabs"href="./extra/E.html" target="_self">E</a>
+    <a class="tabs"href="./extra/F.html" target="_self">F</a>
+    <a class="tabs"href="./extra/G.html" target="_self">G</a>
+    <a class="tabs"href="./extra/H.html" target="_self">H</a>
+    <a class="tabs"href="./extra/I.html" target="_self">I</a>
+    <a class="tabs"href="./extra/J.html" target="_self">J</a>
+    <a class="tabs"href="./extra/K.html" target="_self">K</a>
+    <a class="tabs"href="./extra/L.html" target="_self">L</a>
+    <a class="tabs"href="./extra/M.html" target="_self">M</a>
+    <a class="tabs"href="./extra/N.html" target="_self">N</a>
+    <a class="tabs"href="./extra/O.html" target="_self">O</a>
+    <a class="tabs"href="./extra/P.html" target="_self">P</a>
+    <a class="tabs"href="./extra/Q.html" target="_self">Q</a>
+    <a class="tabs"href="./extra/R.html" target="_self">R</a>
+    <a class="tabs"href="./extra/S.html" target="_self">S</a>
+    <a class="tabs"href="./extra/T.html" target="_self">T</a>
+    <a class="tabs"href="./extra/U.html" target="_self">U</a>
+    <a class="tabs"href="./extra/V.html" target="_self">V</a>
+    <a class="tabs"href="./extra/W.html" target="_self">W</a>
+    <a class="tabs"href="./extra/X.html" target="_self">X</a>
+    <a class="tabs"href="./extra/Y.html" target="_self">Y</a>
+    <a class="tabs"href="./extra/Z.html" target="_self">Z</a>
+    <a class="tabs"href="./extra/0.html" target="_self">0-9</a>`
+  }
+  
+  export default tab;
---
+++ New/extra/script/one.js
@@ -0,0 +1,12 @@
+function B1(){
+
+    return`
+    <span id="sp1">Browse Companies By Name</span> <div id="searchbar"><form class="example" style="margin:auto;max-width:300px">
+        <input type="text" placeholder="Search for a Company" name="search2">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+      </div>`
+}
+
+export default B1;
+
